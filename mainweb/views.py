f754842bb78d4281dda4be77e24b20112df7af1f
Keeper <wls860801@gmail.com>
2012-12-20 02:24:45 +0800

    create the proj files and complete part of page header and footer

---
+++ mainweb/views.py
@@ -0,0 +1,10 @@
+from mainweb.models import *
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import TemplateDoesNotExist
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse,HttpResponseRedirect, HttpResponsePermanentRedirect
+
+
+def index(request):
+    return render_to_response('index.html')
---
+++ urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'eumlab.views.home', name='home'),
+    # url(r'^eumlab/', include('eumlab.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+    (r'^assets/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.ASSETS_ROOT}),
+)
+
+urlpatterns += patterns('mainweb.views',
+    (r'^$', 'index'),
+)
