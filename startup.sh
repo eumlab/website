933e40b44d0c9ce5d8b092cc5264046ac2b02bb4
yxshawn <yx.shawn@gmail.com>
2013-01-23 12:39:46 +0100

    Add startup script. Wrap daw remote hd network page

---
+++ startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PROJDIR="/home/wwwroot/eumlabcom"
+PIDFILE="$PROJDIR/eumlab.pid"
+SOCKET="$PROJDIR/eumlab.sock"
+
+OUTLOGFILE="$PROJDIR/access.log"
+ERRLOGFILE="$PROJDIR/error.log"
+
+cd $PROJDIR
+if [ -f $PIDFILE ]; then
+    kill `cat -- $PIDFILE`
+    rm -f -- $PIDFILE
+fi
+
+exec /usr/bin/env - \
+    PYTHONPATH="../python:.." \
+    ./manage.py runfcgi socket=$SOCKET pidfile=$PIDFILE outlog=$OUTLOGFILE errlog=$ERRLOGFILE umask=0000
+
+
