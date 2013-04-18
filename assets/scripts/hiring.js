055670f3158dbdb1a04fac464f4067ad0ce0dbb4
tinyfive <tinyfive@mycolorway.com>
2013-04-19 01:44:27 +0800

    增加jobs offer的交互

--- assets/scripts/hiring.js
+++ assets/scripts/hiring.js
@@ -19,6 +19,34 @@ $(function() {
     $('.link-jobs').click(function() {
         eum.scrollTo($('.section-5'));
     });
+
+    $('div.staff').click(function(e) {
+        var staff = $(this),
+            keyMap = staff.data('key-map').split(','),
+            introOrder = staff.data('intro');
+            selector = [];
+
+        $.each(keyMap, function(idx, key) {
+            selector.push('[data-key=' + key + ']');
+        });
+        
+        $('.skills').find('.active').removeClass('active')
+            .end().find(selector.join(',')).addClass('active');
+
+        staff.siblings('.selected').removeClass('selected')
+            .end().addClass('selected');
+
+        var oldIntro = $('.intro-inner.selected'),
+            newIntro = $('.intro-inner').eq(introOrder);
+        
+        oldIntro.fadeOut(200, function() {
+            $(this).removeClass('selected')
+
+            newIntro.fadeIn(200, function() {
+                newIntro.addClass('selected');
+            });
+        });
+    });
 });
 
 })(jQuery);
