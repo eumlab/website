9b8268b7bd8d08873c7961c6b1d042cf13b1c4fa
tinyfive <tinyfive@mycolorway.com>
2014-09-14 17:39:30 +0800

    ADD: uke101 lesson animation

--- assets/scripts/uke101.js
+++ assets/scripts/uke101.js
@@ -4,10 +4,13 @@ $(function() {
     eum.loadImage('/assets/images/uke101/part3-l.png');
     eum.loadImage('/assets/images/uke101/part3-r.png');
 
+    // if refresh page, and the position is two perspective section
     twoPerspectiveAnim();
+    lessonAnim();
 
     $(window).scroll(function() {
         twoPerspectiveAnim();
+        lessonAnim();
     });
 });
 
@@ -36,4 +39,18 @@ function adjustTwoPerspectivePos() {
 
     leftEl.css('left', 0 - delta);
     rightEl.css('right', 0 - delta);
+}
+
+function lessonAnim() {
+    var treeEl = $('.tree');
+    var lessonEls = $('.lesson.scale');
+
+    if (treeEl.is('.animated')) return
+
+    var win = $(window);
+
+    if (win.scrollTop() + win.height() < treeEl.offset().top + treeEl.height() * 3 / 5) return
+
+    treeEl.addClass('animated');
+    lessonEls.removeClass('scale');
 }
