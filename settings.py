4bc00a492ac600bb64121c6f3df582b52eb75460
Shawn Xiao <shawn@eumlab.com>
2014-09-15 22:05:03 +0200

    Replace Django Mobile

--- settings.py
+++ settings.py
@@ -110,8 +110,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    "django_mobile.middleware.MobileDetectionMiddleware",
-    "django_mobile.middleware.SetFlavourMiddleware",
     'django_user_agents.middleware.UserAgentMiddleware',
 )
 
@@ -122,7 +120,7 @@ TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
-    'django_mobile.context_processors.flavour'
+    'mainweb.context_processors.mobile',
 )
 
 ROOT_URLCONF = 'urls'
@@ -142,7 +140,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_user_agents',
-    "django_mobile",
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
@@ -172,3 +169,6 @@ LOGGING = {
         },
     }
 }
+
+
+FLAVOURS = ('full' , 'ipad', 'mobile')
